dbb3818a6ef5177e0ed2101b28b3d326061ee17e
Jan Konzal <janq0q0@gmail.com>
2024-03-05 22:37:46 +0100

    add better linux exec

--- build_static_win.sh
+++ build_static_win.sh
@@ -5,5 +5,4 @@
 FILES='src/main.c src/gui.c src/gui.h src/chess.c src/chess.h src/bitboard.c src/bitboard.h src/search.c src/search.h'
 FLAGS='./raylib-win/src/libraylib.a -I./raylib-win/src -lopengl32 -lgdi32 -lwinmm -Wall -O3'
 
-gcc $FILES -Wall -std=c99 -D_DEFAULT_SOURCE -O3 -I./src -I./raylib-w/src -I./raylib-w/src/external -L./src -L./raylib-w/src -L./raylib-w/src -lraylib -lopengl32 -lgdi32 -lwinmm -DPLATFROM_DESKTOP
-
+gcc $FILES -Wall -std=c99 -D_DEFAULT_SOURCE -O3 -I./src -I./raylib-w/src -I./raylib-w/src/external -L./src -L./raylib-w/src -L./raylib-w/src -lraylib -lopengl32 -lgdi32 -lwinmm
